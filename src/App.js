535ec04a4b73d4b23309008281ea12d3aa4c6456
ishantsingla18 <57613954+ishantsingla18@users.noreply.github.com>
2022-06-02 12:04:44 +0530

    added gallery component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Navbar from './components/Navbar/Navbar';
 import LandingScreen from './components/LandingScreen/LandingScreen';
 import Categories from './components/Categories/Categories';
 import Testimonial from './components/Testimonial/Testimonial';
+import Gallery from './components/Gallery/Gallery';
 function App() {
   return (
     <div className="App">
       <Navbar />
       <LandingScreen />
       <Categories />
+      <Gallery />
       <Testimonial />
     </div>
   );
